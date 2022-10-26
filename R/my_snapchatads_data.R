eac6bdfa2de1e86aeca8497fb9a09e6456af05b7
Pablo Sanchez-Martinez <p.sanchez@creaf.uab.cat>
2022-10-26 17:46:51 +0200

    First release to CRAN.

---
+++ R/my_snapchatads_data.R
@@ -0,0 +1,15 @@
+#' Sample of digital marketing data from Snapchat Ads downloaded by means of the Windsor.ai API.
+#'
+#' A dataset containing sample Snapchat ads data
+#' fetched from windsor.ai API. See more at:  https://windsor.ai/
+#'
+#' @format A data frame with 164 rows and 5 variables:
+#' \describe{
+#'   \item{campaign}{name of the campaign}
+#'   \item{clicks}{number of clicks}
+#'   \item{spend}{spend data}
+#'   \item{impressions}{impressions data}
+#'   \item{date}{date}
+#' }
+#' @source \url{https://windsor.ai/}
+"my_snapchatads_data"
